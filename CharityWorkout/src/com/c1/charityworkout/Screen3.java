58028183756b5de9977eb423b39f3de3aca2b5b3
Johan <chj.tsui@gmail.com>
2013-06-10 17:54:46 +0200

    Screen3 gecleand! JAAAAAAAAAAAAA

--- CharityWorkout/src/com/c1/charityworkout/Screen3.java
+++ CharityWorkout/src/com/c1/charityworkout/Screen3.java
@@ -1,6 +1,7 @@
 package com.c1.charityworkout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 //import android.support.v4.view.MotionEventCompat;
@@ -11,99 +12,23 @@ import android.widget.Button;
 //import android.support.v4.view.GestureDetectorCompat;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-<<<<<<< HEAD
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class Screen3 extends Activity {
-    Button thankyou;
-    ImageView imgView;
-    private int y;
-    private int seconds = 55;
-    private int minutes =59;
-    private int hours =0;
-    private String secondenstring = "";
-    private String minutenstring = "";
-=======
 import java.util.*;
 
 public class Screen3 extends Activity {
-    Button thankyou;
-    Button startbutton;
+    Button thankyou, startbutton;
     ImageView imgView;
     private int y;
     private long tijd1;
     private long tijdpauze;
     public static long tijd;
-
->>>>>>> origin/Jeroen
     // private static final String DEBUG_TAG = "Gestures";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_3);
-<<<<<<< HEAD
-        thankyou = (Button) findViewById(R.id.thankyou);
-        // history = (Button) findViewById(R.id.history);
-        thankyou.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent random = new Intent("android.c1.CharityWorkout.THANKYOU");
-                startActivity(random);
-            }
-        });
-        //Declare the timer
-        Timer t = new Timer();
-//Set the schedule function and rate
-        t.scheduleAtFixedRate(new TimerTask() {
-
-            @Override
-            public void run() {
-                //We must use this function in order to change the text view text
-                runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        TextView tv = (TextView) findViewById(R.id.textView);
-                        if(seconds<10){
-                            secondenstring="0"+(String.valueOf(seconds));
-                        }
-                        else{secondenstring=String.valueOf(seconds);
-                        }
-                        minutenstring=String.valueOf(minutes);
-                        if (hours>0){
-                            if(minutes<10){
-                                minutenstring="0"+(String.valueOf(minutes));
-                            }}
-                        tv.setText("Tijd= "+hours+":"+minutenstring+":"+ secondenstring);
-                        seconds += 1;
-                        if (minutes==60){
-                            hours +=1;
-                            minutes = 0;
-                        }if(seconds==60){
-                            seconds = 0;
-                            minutes +=1;
-                        }
-                    }
-
-                });
-            }
-
-        },
-//Set how long before to start calling the TimerTask (in milliseconds)
-                0,
-//Set the amount of time between each execution (in milliseconds)
-                1000);
-
-=======
->>>>>>> origin/Jeroen
-        y = com.c1.charityworkout.MainActivity.x;
-        imgView =(ImageView) findViewById(R.id.imageView2);
-        Drawable image2 = getResources().getDrawable(y);
-        imgView.setImageDrawable(image2);
-
+        rendering();
+        
     }
 
 
@@ -148,9 +73,8 @@ public class Screen3 extends Activity {
         tijd = System.currentTimeMillis()-tijd1;
         thankyou.setText(String.valueOf(tijd));
 
-<<<<<<< HEAD
-
     }
+
     // @Override
     //    public boolean onTouchEvent (MotionEvent event){
     //  float x1, x2, y1, y2;
@@ -166,19 +90,23 @@ public class Screen3 extends Activity {
     //        float differenceInX = x2-x1;
     //        float differenceInY = y2-y1;
 
-    // Use dx and dy to determine the direction
-    //        if(Math.abs(differenceInX) > Math.abs(differenceInY)) {
-    //            if(differenceInX > 0) direction = "right";
-    //            else direction = "left";
-    //        } else {
-    //            if(differenceInY > 0) direction = "down";
-    //           else direction = "up";
-    //       }
-    //       break;
-    //       }
-    //  }
-=======
+	private void rendering() {
+		// TODO Auto-generated method stub
+		thankyou = (Button) findViewById(R.id.thankyou);
+        // history = (Button) findViewById(R.id.history);
+		y = com.c1.charityworkout.MainActivity.x;
+        imgView =(ImageView) findViewById(R.id.imageView2);
+        Drawable image2 = getResources().getDrawable(y);
+        imgView.setImageDrawable(image2);
+        thankyou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent random = new Intent("android.c1.CharityWorkout.THANKYOU");
+                startActivity(random);
+            }
+        });
+	}
 
-    };
->>>>>>> origin/Jeroen
-}
+    }
+
+	
