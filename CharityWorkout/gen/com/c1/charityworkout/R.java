ae572b98184c6a59710a083b0cbd5543c1db7c49
Johan <chj.tsui@gmail.com>
2013-06-13 15:29:54 +0200

    gitignore

--- CharityWorkout/gen/com/c1/charityworkout/R.java
+++ CharityWorkout/gen/com/c1/charityworkout/R.java
@@ -193,27 +193,28 @@ containing a value of this type.
     }
     public static final class id {
         public static final int AmountSet=0x7f060009;
-        public static final int Done=0x7f060015;
+        public static final int Done=0x7f060016;
         public static final int SetAmount=0x7f06000d;
         public static final int Submit=0x7f06000f;
-        public static final int action_settings=0x7f060016;
+        public static final int action_settings=0x7f060017;
         public static final int editText=0x7f06000e;
         public static final int fiftyCent=0x7f06000c;
-        public static final int history=0x7f060013;
+        public static final int gestureOverlayView1=0x7f060010;
+        public static final int history=0x7f060014;
         public static final int homeView=0x7f060005;
         public static final int hybrid=0x7f060004;
         public static final int imageButton=0x7f060006;
         public static final int imageButton2=0x7f060007;
         public static final int imageButton3=0x7f060008;
-        public static final int imageView2=0x7f060010;
+        public static final int imageView2=0x7f060011;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
         public static final int satellite=0x7f060002;
         public static final int tenCent=0x7f06000a;
         public static final int terrain=0x7f060003;
-        public static final int textView=0x7f060011;
-        public static final int thankyou=0x7f060012;
-        public static final int the_map=0x7f060014;
+        public static final int textView=0x7f060012;
+        public static final int thankyou=0x7f060013;
+        public static final int the_map=0x7f060015;
         public static final int twentyCent=0x7f06000b;
     }
     public static final class layout {
