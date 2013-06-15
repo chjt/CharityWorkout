c268abee4ca2c5d1e3028bc5fae1e128f54d3d8a
Johan <chj.tsui@gmail.com>
2013-06-15 22:43:45 +0200

    SplashScreen add, resize imageButtons

---
+++ CharityWorkout/src/com/c1/charityworkout/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.c1.charityworkout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash_screen);
+		
+		Thread waiting = new Thread (){
+			public void run(){
+				try{
+					sleep(5000);				
+				} catch (InterruptedException e){
+					e.printStackTrace();
+				} finally {
+					Intent start = new Intent("android.c1.CharityWorkout.MAINACTIVITY");
+					startActivity(start);
+				}
+			}
+		};
+		waiting.start();
+	}
+
+	
+}
