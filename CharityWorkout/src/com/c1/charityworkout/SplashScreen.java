2e445c3386443259ac2e685599463580ec78cd6b
Johan <chj.tsui@gmail.com>
2013-06-19 22:06:45 +0200

    finish() added @ splash

--- CharityWorkout/src/com/c1/charityworkout/SplashScreen.java
+++ CharityWorkout/src/com/c1/charityworkout/SplashScreen.java
@@ -13,21 +13,20 @@ public class SplashScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.splash_screen);
-		
-		Thread waiting = new Thread (){
-			public void run(){
-				try{
-					sleep(5000);				
-				} catch (InterruptedException e){
-					e.printStackTrace();
-				} finally {
-					Intent start = new Intent("android.c1.CharityWorkout.MAINACTIVITY");
-					startActivity(start);
-				}
-			}
-		};
-		waiting.start();
+				Thread waiting = new Thread() {
+					public void run() {
+						try {
+							sleep(5000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						} finally {
+							Intent start = new Intent(
+									"android.c1.CharityWorkout.MAINACTIVITY");
+							startActivity(start);
+							finish();
+						}
+					}
+				};
+				waiting.start();		
 	}
-
-	
 }
